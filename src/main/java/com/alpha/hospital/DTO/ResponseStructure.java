fa340332db2039c0a1d0fd50c5392a2b5b06dd88
bollamvinayyadav4-pro <bollamvinayyadav4@gmail.com>
2026-02-11 17:45:03 +0530

    okk

---
+++ src/main/java/com/alpha/hospital/DTO/ResponseStructure.java
@@ -0,0 +1,42 @@
+package com.alpha.hospital.DTO;
+
+
+
+	
+
+
+	public class ResponseStructure<T> {
+		private int statusCode;
+		private String message;
+		private T data;
+		public int getStatusCode() {
+			return statusCode;
+		}
+		public void setStatusCode(int statusCode) {
+			this.statusCode = statusCode;
+		}
+		public String getMessage() {
+			return message;
+		}
+		public void setMessage(String message) {
+			this.message = message;
+		}
+		public T getData() {
+			return data;
+		}
+		public void setData(T data) {
+			this.data = data;
+		}
+		public ResponseStructure(int statusCode, String message, T data) {
+			super();
+			this.statusCode = statusCode;
+			this.message = message;
+			this.data = data;
+		}
+		public ResponseStructure() {
+			super();
+		}
+		
+
+	}
+
